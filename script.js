b0c068c29677fe9c1974b64e53a88d7e7fc110c2
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-17 21:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function minCostToFormRope(event)
-{
-	event.preventDefault();
-	let input = document.querySelector("input").value;
-	let arr = input.split(",");
-	arr.sort(function(a,b){return a-b});
+function calculateMinCost(){
+	let ropelengthsStr=document.getElementById('rope-lengths').value;
+	let ropelengthsArr=ropelengthsStr.split(",").map(Number);
+	let n=ropelengthsArr.length;
 
-	let cost = 0;
-	while (arr.length > 1) {
-		let result = Number(arr[0]) + Number(arr[1]);
-		arr.splice(0,2);
-		arr.push(result);
-		cost = cost + result;
-		arr.sort(function(a,b){return a-b});
+let pq=[];
+	for(let i=0;i<n;i++){
+		pq.push(ropelengthsArr[i]);
 	}
-	document.getElementById("result").textContent = cost;
+pq.sort((a,b)=>a-b);
+
+let res=0;
+while(pq.length>1){
+	let first=pq.shift();
+	let second= pq.shift();
+	res+=first+second;
+	pq.push(first+second);
+	pq.sort((a,b)=>a-b);
+}
+	document.getElementById('result').textContent=res;
 }
